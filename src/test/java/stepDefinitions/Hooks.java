1c0aca4001508b95a681ab8cf7599cc72c1901fb
pranjal12dec <pranjal12december@gmail.com>
2023-06-30 17:26:13 +0530

    Setting the OS name and directory dynamically into properties file

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -2,6 +2,12 @@ package stepDefinitions;
 
 import cucumber.TestContext;
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Hooks {
 
@@ -9,6 +15,25 @@ public class Hooks {
 
   public Hooks(TestContext context) {
     testContext = context;
+  }
+
+  @Before
+  public void updatePropertiesfile() throws IOException {
+    Properties extProperties = new Properties();
+    String pathValue = "src/test/resources/extent.properties";
+    FileInputStream inputStream = new FileInputStream(pathValue);
+    extProperties.load(inputStream);
+    inputStream.close();
+
+    String oldVal1 = extProperties.getProperty("systeminfo.OStype");
+    String oldVal2 = extProperties.getProperty("systeminfo.userDirectory");
+
+    extProperties.setProperty("systeminfo.OStype",System.getProperty("os.name"));
+    extProperties.setProperty("systeminfo.userDirectory",System.getProperty("user.dir"));
+
+    FileOutputStream outputStream = new FileOutputStream(pathValue);
+    extProperties.store(outputStream, null);
+    outputStream.close();
   }
 
   @After
